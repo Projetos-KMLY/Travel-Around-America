64cc790a89a1ba1190b14d8cbacf530e5faf2b31
Ma-189 <78057087+Ma-189@users.noreply.github.com>
2021-12-06 00:27:35 -0300

    add

--- main.js
+++ main.js
@@ -16,3 +16,47 @@ for (const link of links) {
     nav.classList.remove('show')
   })
 }
+
+var btn = document.getElementById('clima');
+var c = document.querySelector('.c');
+btn.addEventListener('click', function() {
+    
+  if(c.style.display === 'block') {
+      c.style.display = 'none';
+  } else {
+      c.style.display = 'block';
+  }
+});
+
+var btn = document.getElementById('culinaria');
+var culin = document.querySelector('.culin');
+btn.addEventListener('click', function() {
+    
+  if(culin.style.display === 'block') {
+      culin.style.display = 'none';
+  } else {
+      culin.style.display = 'block';
+  }
+});
+
+var btn = document.getElementById('idioma');
+var idio = document.querySelector('.idio');
+btn.addEventListener('click', function() {
+    
+  if(idio.style.display === 'block') {
+      idio.style.display = 'none';
+  } else {
+      idio.style.display = 'block';
+  }
+});
+
+var btn = document.getElementById('pontos');
+var pont = document.querySelector('.pont');
+btn.addEventListener('click', function() {
+    
+  if(pont.style.display === 'block') {
+      pont.style.display = 'none';
+  } else {
+      pont.style.display = 'block';
+  }
+});
