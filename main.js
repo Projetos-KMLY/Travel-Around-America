7b59324acc84583afa6fa161ec397002eac9bd6d
Ma-189 <78057087+Ma-189@users.noreply.github.com>
2021-12-06 20:58:46 -0300

    add

--- main.js
+++ main.js
@@ -17,6 +17,8 @@ for (const link of links) {
   })
 }
 
+/* esconder caracteristicas */
+
 var btn = document.getElementById('clima');
 var c = document.querySelector('.c');
 btn.addEventListener('click', function() {
@@ -60,3 +62,90 @@ btn.addEventListener('click', function() {
       pont.style.display = 'block';
   }
 });
+
+/* mudar cor ao clicar categorias */
+
+let myButton = document.querySelectorAll('.categorias > button');
+
+myButton.forEach(function(key){
+    key.addEventListener('click', function(){
+        removeStyles();
+        this.setAttribute('class', 'buttonClicked');
+    });
+})
+
+function removeStyles(){
+    for(let i = 0;i < myButton.length;i++){
+        document.querySelectorAll('.categorias > button')[i].removeAttribute('class');
+    }
+}
+
+/* mudar cor ao clicar clima*/
+
+let myButton1 = document.querySelectorAll('.c > button');
+
+myButton1.forEach(function(key){
+    key.addEventListener('click', function(){
+        removeStyles1();
+        this.setAttribute('class', 'buttonClicked2');
+    });
+})
+
+function removeStyles1(){
+    for(let i = 0;i < myButton1.length;i++){
+        document.querySelectorAll('.c > button')[i].removeAttribute('class');
+    }
+}
+
+/* mudar cor ao clicar culinaria */
+
+let myButton2 = document.querySelectorAll('.culin > button');
+
+myButton2.forEach(function(key){
+    key.addEventListener('click', function(){
+        removeStyles2();
+        this.setAttribute('class', 'buttonClicked2');
+    });
+})
+
+function removeStyles2(){
+    for(let i = 0;i < myButton3.length;i++){
+        document.querySelectorAll('.culin > button')[i].removeAttribute('class');
+    }
+}
+
+/* mudar cor ao clicar idioma*/
+
+let myButton3 = document.querySelectorAll('.idio > button');
+
+myButton3.forEach(function(key){
+    key.addEventListener('click', function(){
+        removeStyles3();
+        this.setAttribute('class', 'buttonClicked2');
+    });
+})
+
+function removeStyles3(){
+    for(let i = 0;i < myButton3.length;i++){
+        document.querySelectorAll('.idio > button')[i].removeAttribute('class');
+    }
+}
+
+/* mudar cor ao clicar pontos turisticos */
+
+let myButton4 = document.querySelectorAll('.pont > button');
+
+myButton4.forEach(function(key){
+    key.addEventListener('click', function(){
+        removeStyles4();
+        this.setAttribute('class', 'buttonClicked2');
+    });
+})
+
+function removeStyles4(){
+    for(let i = 0;i < myButton4.length;i++){
+        document.querySelectorAll('.pont > button')[i].removeAttribute('class');
+    }
+}
+
+/* mudar cor ao clicar categorias */
