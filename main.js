ff0a8a41eab9cc2cb1e52d2ef90379e3283fe4b2
bonfim2003 <yasmin.bonfim.0310@gmail.com>
2021-12-08 22:12:41 -0300

    ok

--- main.js
+++ main.js
@@ -19,12 +19,35 @@ for (const link of links) {
 
 /*----MAPA AMERICA-----*/
 
-import * as d3 from 'd3'
+const width = 900
+const height = 600
 
-var map = new Datamap({
-  element: document.getElementById('america'),
-  scope: 'america'
-})
+const svg = d3
+  .select('body')
+  .append('svg')
+  .attr('width', width)
+  .attr('height', height)
+
+const projection = d3
+  .geoMercator()
+  .scale(140)
+  .translate([width / 2, height / 1.4])
+const path = d3.geoPath(projection)
+
+const g = svg.append('g')
+
+d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json').then(
+  data => {
+    const countries = topojson.feature(data, data.objects.countries)
+    g.selectAll('path')
+      .data(countries.features)
+      .enter()
+      .append('path')
+      .attr('class', 'country')
+      .attr('d', path)
+  }
+)
+
 /*
 const itens_caracteristica = querySelectorAll(".item-caracteristica") 
 itens_caracteristica.addEventListener('click', function (event){})
