1983e8dff1d22812217b40b0065a3ef1ec331bc7
bonfim2003 <yasmin.bonfim.0310@gmail.com>
2021-12-15 00:39:32 -0300

    ok

--- telas.php
+++ telas.php
@@ -25,6 +25,12 @@ endif;
 <link rel="stylesheet" href="telas.css" />
 
 <!-- Fonts -->
+
+<link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css"
+    />
+
 <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 <link
@@ -36,19 +42,11 @@ endif;
 <body>
 
     <main>
-        <div class="modal-container" id="modal_container">
-            <div id="paises">
+        
+    <div class="modal-container">
+    <div class="card">
+  <div class="products">
 
-            </div>
-            <div id="curiosidade">
-
-            </div>
-            <div id="foto">
-
-            </div>
-            <div class="modal">
-
-            <!-- FILTRO PAISES -->
             <?php
             $escolha= $_GET['escolha'];
            
@@ -59,50 +57,78 @@ endif;
             
                 while($getpaisesfiltrados = mysqli_fetch_array($getpaisesquery)){
                     $paises = $getpaisesfiltrados['Id_pais'];
-                    echo "<center>$paises</center>";
+                    // echo "<center>$paises</center>";
 
                     // Aparecer caracteristicas
 
                     $sql = "SELECT * FROM pais WHERE Id_pais = '$paises'";
                     $sth = $conexao->query($sql);
                     $result=mysqli_fetch_array($sth);
+                    // echo $result['text1'];
+                    ?>
 
-                    echo $result['text1'];
 
-                    echo '<img style="width:128px;height:128px" src="data:image/jpeg;base64,'.base64_encode( $result['img1'] ).'"/>';
+    <div class="product" product-id="1">
+    <div class="thumbnail">
+      <?php   echo '<img class="imgcarac" style="width:128px;height:128px" src="data:image/jpeg;base64,'.base64_encode( $result['img1'] ).'"/>'; ?>
+      </div class="description">
+ <h1> <?php echo $result['text1']; ?></h1>
 
-                    echo $result['text2'];
+    </div>
 
-                    echo '<img style="width:128px;height:128px" src="data:image/jpeg;base64,'.base64_encode( $result['img2'] ).'"/>';
+    <div class="product " product-id="2" >
+      <div class="thumbnail">
+      <?php   echo '<img class="imgcarac" style="width:128px;height:128px" src="data:image/jpeg;base64,'.base64_encode( $result['img2'] ).'"/>'; ?>
+      </div>
+      <h1 class="description"> <?php echo $result['text2']; ?></h1>
+    </div>
 
-                    echo $result['text3'];
+    <div class="product" product-id="3">
+    <div class="thumbnail">
+      <?php   echo '<img style="width:128px;height:128px" src="data:image/jpeg;base64,'.base64_encode( $result['img3'] ).'"/>'; ?>
+      </div>
+      <h1 class="description"> <?php echo $result['text3']; ?></h1>
+    </div>
 
-                    echo '<img style="width:128px;height:128px" src="data:image/jpeg;base64,'.base64_encode( $result['img3'] ).'"/>';
+    <div class="product" product-id="4" >
+    <div class="thumbnail">
+      <?php   echo '<img style="width:128px;height:128px" src="data:image/jpeg;base64,'.base64_encode( $result['img4'] ).'"/>'; ?>
+      </div>
+      <h1 class="description"> <?php echo $result['text4']; ?></h1>
+    </div>
 
-                    echo $result['text4'];
+    <div class="product" product-id="5" >
+    <div class="thumbnail">
+      <?php   echo '<img style="width:128px;height:128px" src="data:image/jpeg;base64,'.base64_encode( $result['img5'] ).'"/>'; ?>
+      </div>
+      <h1 class="description"> <?php echo $result['text5']; ?></h1>
+    </div>
+    
+  
+  
+                 
 
-                    echo '<img style="width:128px;height:128px" src="data:image/jpeg;base64,'.base64_encode( $result['img4'] ).'"/>';
-
-                    echo $result['text5'];
-
-                    echo '<img style="width:128px;height:128px" src="data:image/jpeg;base64,'.base64_encode( $result['img5'] ).'"/>';
-                
+<?php
                 }
-            ?>
-           
-                <div class="opi">
+            
+           ?>
+
+</div>
+<div class="opi">
               <button id="coracao"><img class="core" src="./img/coracao.png" /></button>
             <button id="dislike"><img class="like" src="./img/dislike.png" /></button>
         </div>
         <div class="pular">
-            <button id="esquerda"><img class="dire" src="./img/esquerda.png" /></button>
+            <a id="prev" ripple="" class="btn"><img class="dire" src="./img/esquerda.png" /></a>
             <button id="proximo">Próximo país</button>
-            <button id="direita"><img class="esquer" src="./img/direita.png" /></button>
-        </div>
-            </div>
-            </div>
-    </main>
+            <a id="next" ripple="" class="btn"><img class="esquer" src="./img/direita.png" /></a>
+        </div> 
+              </div>
 
+
+    </main>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="./telas.js"></script>
 </body>
 
 </html>
